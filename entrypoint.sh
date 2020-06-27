c7aa18312b701f4c74abb23029787a397d5becd0
Laeyoung <gadise@gmail.com>
2020-06-27 20:20:14 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,7 @@
-cd /app/codes/models/modules/DCNv2
-
 export CUDA_HOME=/usr/local/cuda-10.0
 export CUDNN_INCLUDE_DIR=/usr/local/cuda-10.0/include
 export CUDNN_LIB_DIR=/usr/local/cuda-10.0/lib64
 
+cd /app/codes/models/modules/DCNv2
+
 bash make.sh
-
-cd /app
-
-jupyter lab --ip=0.0.0.0 --port=80 --allow-root --no-browser
